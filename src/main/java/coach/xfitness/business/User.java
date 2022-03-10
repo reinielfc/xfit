a405263966d0e7dc42a581ade4d9e48da06a380d
Reiniel Fernandez <reinielfc@gmail.com>
2022-03-10 21:11:02 +0000

    create User entity bean

---
+++ src/main/java/coach/xfitness/business/User.java
@@ -0,0 +1,60 @@
+package coach.xfitness.business;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ * User
+ */
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userID;
+    private String name;
+    private String email;
+    private String password;
+
+    public User() {
+        this.name = "";
+        this.email = "";
+        this.password = "";
+    }
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
