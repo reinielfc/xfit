4b5c6d3b4231c1cb3d5661e6f0716569b91759b1
JHerdocia112 <JHerdocia112@gmail.com>
2022-03-23 02:39:37 +0000

    add Autologin

--- src/main/java/coach/xfitness/controller/UserController.java
+++ src/main/java/coach/xfitness/controller/UserController.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import coach.xfitness.business.User;
 import coach.xfitness.data.UserDB;
@@ -23,10 +25,42 @@ public class UserController extends HttpServlet {
             url = signUp(request, response);
         } else if (requestURI.endsWith("/login")) {
             url = logIn(request, response);
+        
+        //AUTOLOGIN
+        } else if (requestURI.endsWith("/" /* TO BE DECIDED */)){
+            url = autolog(request, response);
         }
         getServletContext()
                 .getRequestDispatcher(url)
                 .forward(request, response);
+    }
+
+    private String autolog(HttpServletRequest request, HttpServletResponse response){
+       String url = "";
+       String email;
+        HttpSession session = request.getSession();
+        User user = (User) request.getAttribute("user");
+        if(user == null){
+            Cookie get[] = request.getCookies();
+            email = CookieUtil.getCookieValue(get, "loginCookie");
+            
+            if(email.equals("") || email == null){
+                url = "/index.jsp";
+                return url;
+            }
+            else{
+                user = UserDB.selectUser(email);
+
+                if(user == null){
+                    url = "/index.jsp";
+                    return url;
+                }
+                session.setAttribute("user", user);
+            }
+        }
+
+        url = ""; //URL FOR TODAYS WORKOUT
+        return url;
     }
 
     private String signUp(HttpServletRequest request, HttpServletResponse response) {
@@ -51,7 +85,7 @@ public class UserController extends HttpServlet {
             UserDB.insert(user);
             message = "";
             request.setAttribute("message", message);
-            url = "/"; // TODO: add URL
+            url = "/"; // EMAIL CONFIRMATION LINK
         }
 
         return url;
@@ -61,12 +95,16 @@ public class UserController extends HttpServlet {
         String url, message;
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-
+        
         if(UserDB.hasUser(email)){
             User search = UserDB.selectUser(email);
             
             if(true/*PasswordUtil.validate(password, search.getPassword())*/){
                 url = "/"; //Direct to Todays workout page
+                Cookie log1 = new Cookie("loginCookie", search.getEmail());
+                log1.setMaxAge(60*60*24*3); //cookie max age is 2 days
+                log1.setPath("/"); //path for cookie to be used by entire application
+                response.addCookie(log1); //saves cookie to client pc
             }
             else{
                 message = "Invalid Password.";
