9f06418abb5fb59bdf71b5bbf3e1f0780ee4bfde
Reiniel Fernandez <reinielfc@gmail.com>
2022-03-29 05:09:01 +0000

    finish UserDB

--- src/main/java/coach/xfitness/data/DBUtil.java
+++ src/main/java/coach/xfitness/data/DBUtil.java
@@ -2,6 +2,9 @@ package coach.xfitness.data;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class DBUtil {
     private static final EntityManagerFactory entityManagerFactory = 
@@ -10,4 +13,13 @@ public class DBUtil {
     public static EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;
     }
+
+    public static <T> List<T> castList(Class<? extends T> _class, Collection<?> collection) {
+        List<T> list = new ArrayList<>(collection.size());
+        for (Object object: collection) {
+            list.add(_class.cast(object));
+        }
+        return list;
+    }
+
 }
--- src/main/java/coach/xfitness/data/UserDB.java
+++ src/main/java/coach/xfitness/data/UserDB.java
@@ -1,77 +1,112 @@
 package coach.xfitness.data;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-import javax.persistence.NoResultException;
-import javax.persistence.NonUniqueResultException;
-import javax.persistence.TypedQuery;
-
+import coach.xfitness.business.Exercise;
 import coach.xfitness.business.User;
 
 import javax.persistence.*;
 
 public class UserDB {
+    // TODO: This method is for tesing purposes only, DELETE IT
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("jsmith");
+        user.setEmail("jsmith@example.net");
+        user.setPassword("sesame");
 
-    public static void insert(User user) {
-        EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
-        try {
-            if(hasUser(user.getEmail())){
-                throw new NonUniqueResultException();
-            }
-            else{
-                entityManager.persist(user);
-                entityTransaction.commit();
-            }
-        } catch (Exception e) {
-            System.out.println("Failed to add User.");
-        } finally {
-            entityManager.close();
-        }
+        UserDB.insert(user);
+
+        System.out.println(
+            UserDB.select(user.getEmail()).getEmail()
+        );;
+
+        user.setName("john");
+        UserDB.update(user);
+
+        System.out.println(
+            UserDB.select(user.getEmail()).getEmail()
+        );
+
+        user = UserDB.select(user.getEmail());
+        user.setEmail("jsmith@email.com");
+        UserDB.update(user);
+
+        System.out.println(
+            UserDB.select(user.getEmail()).getEmail()
+        );
+
+        user.getFavoriteExercises().add(ExerciseDB.select(22));
+        user.getFavoriteExercises().add(ExerciseDB.select(32));
+        user.getFavoriteExercises().stream().forEach(fe -> {
+            fe.getExerciseMusclesById().forEach(em -> System.out.println(fe.getTitle() + ": " + em.getMuscleByMuscleId().getName()));
+        });
+
+        UserDB.delete(user.getEmail());
+
+        System.out.println(
+            UserDB.select(user.getEmail()).getEmail()
+        );
     }
 
-    public static User selectUser(String email) {
+    public static User select(String email) {
         EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
+        TypedQuery<User> typedQuery = entityManager.createNamedQuery("User.selectByEmail", User.class);
+        typedQuery.setParameter("email", email);
         User result = null;
-        try{
-            String queryString = "SELECT user FROM User user WHERE user.email = :email";
-            TypedQuery<User> typedQuery = entityManager.createQuery(queryString, User.class);
-            typedQuery.setParameter("email", email);
+
+        try {
+            System.out.println("[DATABASE] SELECT: User with email '" + email + "' from database.");
             result = typedQuery.getSingleResult();
         } catch (NoResultException e) {
-            System.out.println("Could not find user in database.");
-        } finally {
-            entityManager.close();
+            System.err.println("[DATABASE ERROR] SELECT FAIL: User with email '" + email + "' not found:");
+            System.err.println(e);
         }
 
         return result;
     }
 
-    public static boolean hasUser(String email) {
-        User user = selectUser(email);
-        return user != null;
-    }  
-
-    public static void deleteUser(String email){
+    public static void insert(User user) {
         EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
-        entityManager.getTransaction().begin();
-        try{
-            User s = selectUser(email);
-            User u = entityManager.find(User.class, s.getId());
-           entityManager.remove(u);
-           entityManager.getTransaction().commit();
-        }
-        catch(Exception e){
-            System.out.println("Couldn't delete User");
-        }
-        finally{
-            entityManager.close();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        String email = user.getEmail();
+
+        try {
+            System.out.println("[DATABASE] INSERT: User with email '" + email  + "' to database.");
+            if (UserDB.has(email)) {
+                System.err.println("[DATABASE ERROR] INSERT FAIL: User with email '" + email + "' already exists:");
+                throw new NonUniqueResultException();
+            } else {
+                entityManager.persist(user);
+                entityTransaction.commit();
+            }
+        } catch (Exception e) {
+            System.err.println("[DATABASE ERROR] INSERT FAIL: User with email '" + email + "' could not be inserted:");
+            System.err.println(e);
         }
     }
 
-    public static boolean updateUser( User user) {
-        return true;
+    public static boolean has(String email) {
+        User user = select(email);
+        return user != null;
+    }
+
+    public static void update(User user) {
+        EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        System.out.println("[DATABASE] UPDATE: User with email '" + user.getEmail() + "'.");
+        entityManager.merge(user);
+        entityManager.getTransaction().commit();
+    }
+
+    public static void delete(String email) {
+        EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+
+        User user = entityManager.find(User.class, UserDB.select(email).getId());
+        System.out.println("[DATABASE] DELETE: User with email '" + user.getEmail() + "'.");
+        entityManager.remove(user);
+        entityManager.getTransaction().commit();
     }
 
 }
