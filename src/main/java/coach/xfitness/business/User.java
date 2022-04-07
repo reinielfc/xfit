3d10f7d55fb64a1405e28ca0caa805f4149c3cb2
Reiniel Fernandez <reinielfc@gmail.com>
2022-04-07 15:12:31 +0000

    reorganize imports

--- src/main/java/coach/xfitness/business/User.java
+++ src/main/java/coach/xfitness/business/User.java
@@ -3,14 +3,7 @@ package coach.xfitness.business;
 import java.util.Collection;
 import java.util.Objects;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 
 @Entity
 @NamedQuery(name = "User.selectByEmail", query = "SELECT u FROM User u WHERE u.email = :email")
