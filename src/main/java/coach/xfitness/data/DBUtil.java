500f8f1768c850180f65a3a97e57acdcabd74e6d
Reiniel Fernandez <reinielfc@gmail.com>
2022-04-07 15:16:04 +0000

    refactor

--- src/main/java/coach/xfitness/data/DBUtil.java
+++ src/main/java/coach/xfitness/data/DBUtil.java
@@ -8,8 +8,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
-    private static final EntityManagerFactory entityManagerFactory = 
-        Persistence.createEntityManagerFactory("default");
+    private static final EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("default");
 
     public static EntityManagerFactory getEntityManagerFactory() {
         return entityManagerFactory;
--- src/main/java/coach/xfitness/data/UserDB.java
+++ src/main/java/coach/xfitness/data/UserDB.java
@@ -10,7 +10,7 @@ import javax.persistence.TypedQuery;
 import coach.xfitness.business.User;
 public class UserDB {
 
-    public static User select(String email) {
+    public static User selectByEmail(String email) {
         EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
         TypedQuery<User> typedQuery = entityManager.createNamedQuery("User.selectByEmail", User.class);
         typedQuery.setParameter("email", email);
@@ -21,7 +21,7 @@ public class UserDB {
             result = typedQuery.getSingleResult();
         } catch (NoResultException e) {
             System.err.println("[DATABASE ERROR] SELECT FAIL: User with email '" + email + "' not found:");
-            System.err.println(e);
+            e.printStackTrace();
         }
 
         return result;
@@ -45,12 +45,12 @@ public class UserDB {
             }
         } catch (Exception e) {
             System.err.println("[DATABASE ERROR] INSERT FAIL: User with email '" + email + "' could not be inserted:");
-            System.err.println(e);
+            e.printStackTrace();
         }
     }
 
     public static boolean has(String email) {
-        User user = select(email);
+        User user = selectByEmail(email);
         return user != null;
     }
 
@@ -62,11 +62,11 @@ public class UserDB {
         entityManager.getTransaction().commit();
     }
 
-    public static void delete(String email) {
+    public static void deleteByEmail(String email) {
         EntityManager entityManager = DBUtil.getEntityManagerFactory().createEntityManager();
         entityManager.getTransaction().begin();
 
-        User user = entityManager.find(User.class, UserDB.select(email).getId());
+        User user = entityManager.find(User.class, UserDB.selectByEmail(email).getId());
         System.out.println("[DATABASE] DELETE: User with email '" + user.getEmail() + "'.");
         entityManager.remove(user);
         entityManager.getTransaction().commit();
