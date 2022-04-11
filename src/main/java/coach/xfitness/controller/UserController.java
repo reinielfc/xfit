7a4210eec81859b08125a1b7cc548567a80bf2f0
Reiniel Fernandez <reinielfc@gmail.com>
2022-04-11 21:18:10 +0000

    implement registration

--- src/main/java/coach/xfitness/controller/UserController.java
+++ src/main/java/coach/xfitness/controller/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
@@ -48,43 +49,164 @@ public class UserController extends HttpServlet {
 
     // #region register
 
-    private String register(HttpServletRequest request, HttpServletResponse response) {
+    private String register(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        String url = "/register.jsp";
+
+        if (action.equals("verify")) {
+            url = verifyAction(request);
+        } else {
+            url = registerAction(request, response);
+        }
+
+        return url;
+    }
+
+    // TODO: change name to something more descriptive?
+    private String registerAction(HttpServletRequest request, HttpServletResponse response) {
+        User newUser = makeFromRequest(request);
+        String validationMessage = validate(newUser);
+
+        if (validationMessage.isBlank()) {
+            try {
+                securePassword(newUser);
+
+                // save new user in session so it's accessible by email servlet
+                HttpSession session = request.getSession();
+                session.setAttribute("recipient", newUser);
+
+                sendVerificationEmail(request, response);
+
+                return "/verify-email.jsp";
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                e.printStackTrace();
+                validationMessage = "An error has occurred. Please try again later.";
+            } catch (ServletException | IOException e) {
+                e.printStackTrace();
+                validationMessage = "Verification email could not be sent. Please try again later.";
+            }
+        }
+
+        request.setAttribute("validationMessage", validationMessage);
+
+        return "/register.jsp";
+    }
+
+    // TODO: change name to something more descriptive?
+    private String verifyAction(HttpServletRequest request) {
+        String validationMessage = "Email verification was unsuccessful.";
+        String url = "/verify-email.jsp";
+
+        boolean codeIsVerified = verifyEmailedCode(request);
+
+        if (codeIsVerified) {
+            // get user from session
+            HttpSession session = request.getSession(false);
+            User newUser = (User) session.getAttribute("recipient");
+
+            // register user
+            UserDB.insert(newUser);
+
+            // TODO: sign user in
+            session.setAttribute("user", newUser);
+
+            // remove new user
+            session.removeAttribute("recipient");
+
+            validationMessage = "Email verification was successful.";
+            url = "/equipment.jsp";
+        }
+
+        request.setAttribute("validationMessage", validationMessage);
+
+        return url;
+    }
+
+    private User makeFromRequest(HttpServletRequest request) {
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
 
-        User user = new User();
-        user.setName(name);
-        user.setEmail(email);
+        User newUser = new User();
+        newUser.setName(name);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+
+        return newUser;
+    }
+
+    private void securePassword(User user) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String password = PasswordUtil.generate(user.getPassword());
         user.setPassword(password);
+    }
 
-        request.setAttribute("user", user);
+    private String validate(User user) {
+        String validationMessage = validatePassword(user.getPassword());
 
-        String url = "";
-        String message = "";
-        if (UserDB.hasUserWithEmail(email)) {
-            message = "This email address is already in use.";
-        } else {
-            try {
-                String hashedPassword = PasswordUtil.generate(password);
-                user.setPassword(hashedPassword);
-                HttpSession session = request.getSession();
-                session.setAttribute("newUser", user);
-                message = "";
-            } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
-                System.out.println(e);
-                // url = "?"; // TODO: add error page
-            }
-            message = "";
-            request.setAttribute("message", message);
-            //before url is set to email validation, email the user with a randomly generated code and store it in the session
-            EmailController.CodeEmail(request, response);
-            url = "/email-verification.jsp"; //Email confirmation link
+        if (!validationMessage.isBlank())
+            return validationMessage;
+
+        return validateEmail(user.getEmail());
+    }
+
+    private static String validatePassword(String password) {
+        String validationMessage = "";
+
+        if (!PasswordUtil.validate(password)) {
+            validationMessage = "Password is not strong enough! "
+                    + "Make sure it is at least 8 characters long, and "
+                    + "include at least 1 character from A-Z, a-z, and 0-9.";
         }
 
-        request.setAttribute("message", message);
+        return validationMessage;
+    }
 
-        return url;
+    private static String validateEmail(String email) {
+        String validationMessage = "";
+
+        if (UserDB.hasUserWithEmail(email)) {
+            validationMessage = "\"" + email + "\" is already in use.";
+        }
+
+        return validationMessage;
+    }
+
+    private void sendVerificationEmail(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // generate email verification code
+        String verificationCode = PasswordUtil.generateCode();
+
+        // send code email
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/email");
+        HttpSession session = request.getSession(false);
+
+        // store code in session
+        session.setAttribute("emailVerificationCode", verificationCode);
+
+        requestDispatcher.include(request, response);
+    }
+
+    private boolean verifyEmailedCode(HttpServletRequest request) {
+        // get session
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return false;
+        }
+
+        // get sent code from session
+        String emailVerificationCode = (String) session.getAttribute("emailVerificationCode");
+        if (emailVerificationCode == null || emailVerificationCode.isEmpty()) {
+            return false;
+        }
+
+        // get and verify user input code from request
+        String code = request.getParameter("code");
+        if (code == null || code.isBlank() || !code.equals(emailVerificationCode)) {
+            return false;
+        }
+
+        return true;
     }
 
     // #endregion register
