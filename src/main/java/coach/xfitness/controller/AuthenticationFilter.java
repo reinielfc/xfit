93950f97d00e2e2dbe55e42448765c3d0396f780
Reiniel Fernandez <reinielfc@gmail.com>
2022-05-04 04:32:05 +0000

    move authentication filter to controller

--- src/main/java/coach/xfitness/controller/AuthenticationFilter.java
+++ src/main/java/coach/xfitness/controller/AuthenticationFilter.java
@@ -1,11 +1,10 @@
-package coach.xfitness.interceptor;
+package coach.xfitness.controller;
 
 import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -19,9 +18,17 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        
+
     }
 
+    /**
+     * If the user is not authenticated, redirect to the authentication page
+     * 
+     * @param servletRequest The request object that is passed to the servlet.
+     * @param servletResponse The response object that is passed to the filter.
+     * @param chain The FilterChain object that represents the filter chain to which
+     * this filter belongs.
+     */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
             throws IOException, ServletException {
@@ -29,9 +36,10 @@ public class AuthenticationFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpSession session = request.getSession(false);
 
-        if (session == null) {
-            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/authenticate");
-            requestDispatcher.include(request, response);
+        if (session == null || session.getAttribute("user") == null) {
+            request.setAttribute("authenticate", true);
+            request.getRequestDispatcher("/authenticate")
+                    .include(request, response);
         }
 
         chain.doFilter(request, response);
