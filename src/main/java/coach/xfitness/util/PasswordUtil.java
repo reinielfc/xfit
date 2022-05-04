f9001e51eb92f79a8b935c49db69cc4cf280cc84
Reiniel Fernandez <reinielfc@gmail.com>
2022-05-04 03:28:04 -0400

    remove password validation (moved to UserDB)

--- src/main/java/coach/xfitness/util/PasswordUtil.java
+++ src/main/java/coach/xfitness/util/PasswordUtil.java
@@ -4,10 +4,8 @@ import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Arrays;
 import java.util.Base64;
 import java.util.Random;
-import java.util.regex.Pattern;
 
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
@@ -16,23 +14,11 @@ public class PasswordUtil {
     private static final SecureRandom SECURE_RANDOM = new SecureRandom();
     private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder();
 
-    public static boolean validate(String password) {
-        if (password == null || password.isBlank()) {
-            return false;
-        }
-
-        String[] requirements = new String[] { "\\S{8,}", "[A-Z]", "[a-z]", "[0-9]" };
-
-        for (String requirement : requirements) {
-            // if password does not match requirement regex
-            if (!Pattern.compile(requirement).matcher(password).find()) {
-                return false; // it is invalid
-            }
-        }
-
-        return true;
-    }
-
+    /**
+     * Generate a random 6-digit number and return it as a string.
+     * 
+     * @return A random 6 digit number
+     */
     public static String generateCode() {
         int code = new Random().nextInt(999999);
         return String.format("%06d", code);
